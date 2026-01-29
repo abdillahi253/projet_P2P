a57f44ec2ac0909d7e230845ea634dc54da23681
abdillahi253 <demabdillahi2002@gmail.com>
2026-01-29 21:27:33 +0100

    Refactory v2

---
+++ peer/peer_client.py
@@ -0,0 +1,52 @@
+import socket
+import json
+import os
+from protocol import *
+
+CENTRAL_HOST = "192.168.2.61"
+CENTRAL_PORT = 9090
+SHARED_DIR = "shared"
+
+def search(sock, keyword):
+    sock.send(f"{SEARCH} {keyword}".encode(ENCODING))
+    data = sock.recv(BUFFER_SIZE).decode(ENCODING)
+    return json.loads(data)
+
+def download(owner, port, filename):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((owner, port))
+    s.send(f"{GET} {filename}".encode(ENCODING))
+
+    # S'assurer que le dossier 'shared' existe
+    os.makedirs(SHARED_DIR, exist_ok=True)
+    path = os.path.join(SHARED_DIR, filename)
+    with open(path, "wb") as f:
+        while data := s.recv(BUFFER_SIZE):
+            f.write(data)
+    s.close()
+
+def connect_to_central_server():
+    print(f"Connexion au serveur central ({CENTRAL_HOST}:{CENTRAL_PORT})...")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((CENTRAL_HOST, CENTRAL_PORT))
+    return sock
+
+def main():
+    sock = connect_to_central_server()
+    if not sock:
+        return
+
+    keyword = input("Mot-clé: ")
+    results = search(sock, keyword)
+
+    print("Résultats:")
+    for r in results:
+        print(r)
+
+    if results:
+        r = results[0]
+        download(r['owner'], r['port'], r['filename'])
+
+
+if __name__ == "__main__":
+    main()
